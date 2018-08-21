310e2d0943ea8e00a8b3e53ee721b265eab19824
Rainboz <784269332@qq.com>
2018-08-21 18:33:15 +0800

    二叉树队列

---
+++ src/com/zhb/queue/Solution.java
@@ -0,0 +1,19 @@
+package com.zhb.queue;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author Rainboz E-mail:784269332@qq.com
+ * @version 创建时间：2018年8月21日 下午5:39:32 类说明TODO
+ */
+public class Solution {
+	private Queue<Integer> queue = new ArrayDeque<Integer>();
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		
+		return null;
+	}
+}
