7a0d5f05085d38cf2bbc2b945ba536af7412f119
rainboz <784269332@qq.com>
2019-04-23 00:29:36 +0800

    ［需求／BUG］:单元测试
    ［模块］:
    
    ［说明］：

--- src/test/java/com/zhb/javaSE/package201904/package_object/extends_test/AccountTest.java
+++ src/test/java/com/zhb/javaSE/package201904/package_object/extends_test/AccountTest.java
@@ -1,5 +1,17 @@
+package com.zhb.javaSE.package201904.package_object.extends_test;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AccountTest {
+
+    @Test
+    public void method1(){
+        Account account = new Account(1122,20000,4.5);
+        account.withdraw(30000);
+        account.withdraw(2500);
+        account.deposit(3000);
+    }
 
 }
--- src/test/java/com/zhb/javaSE/package201904/package_object/extends_test/CheckAccountTest.java
+++ src/test/java/com/zhb/javaSE/package201904/package_object/extends_test/CheckAccountTest.java
@@ -1,5 +1,16 @@
+package com.zhb.javaSE.package201904.package_object.extends_test;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CheckAccountTest {
 
+    @Test
+    public void withdraw() {
+        CheckAccount c = new CheckAccount(1122,20000,4.5,5000);
+        c.withdraw(5000);
+        c.withdraw(18000);
+        c.withdraw(3000);
+    }
 }
