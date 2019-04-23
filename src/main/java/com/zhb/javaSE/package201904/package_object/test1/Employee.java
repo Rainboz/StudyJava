fb80457d86d41374c0675fba769f3311095c8adb
rainboz <784269332@qq.com>
2019-04-23 11:10:58 +0800

    ［需求／BUG］:实验一
    ［模块］:
    
    ［说明］：

---
+++ src/main/java/com/zhb/javaSE/package201904/package_object/test1/Employee.java
@@ -0,0 +1,13 @@
+package com.zhb.javaSE.package201904.package_object.test1;
+
+/**
+ * @program: StudyJava
+ * @description:
+ * @author: rainboz
+ * @create: 2019-04-23 10:51
+ **/
+public class Employee {
+    private String name;
+    private int age;
+    private MyDate birthday;
+}
---
+++ src/main/java/com/zhb/javaSE/package201904/package_object/test1/MyDate.java
@@ -0,0 +1,10 @@
+package com.zhb.javaSE.package201904.package_object.test1;
+
+/**
+ * @program: StudyJava
+ * @description:
+ * @author: rainboz
+ * @create: 2019-04-23 11:09
+ **/
+public class MyDate {
+}
