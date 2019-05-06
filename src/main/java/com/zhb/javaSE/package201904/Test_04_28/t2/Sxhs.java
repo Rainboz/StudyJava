c64d996d33b324f1e9b35bbe6869e5afe12a0657
Rainboz <784269332@qq.com>
2019-05-06 09:46:34 +0800

    4月考试

--- src/main/java/com/zhb/javaSE/package201904/Test_04_28/t2/Sxhs.java
+++ src/main/java/com/zhb/javaSE/package201904/Test_04_28/t2/Sxhs.java
@@ -27,18 +27,28 @@ public class Sxhs {
             char c1 = str.charAt(0);
             char c2 = str.charAt(1);
             char c3 = str.charAt(2);
-
-            int b1 = c1 * c1 * c1;
-            int b2 = c2 * c2 * c2;
-            int b3 = c3 * c3 * c3;
+            //char-->string-->int
+//            int i1 = Integer.valueOf(Character.toString(c1));
+//            int i2 = Integer.valueOf(Character.toString(c2));
+//            int i3 = Integer.valueOf(Character.toString(c3));
+            //char转int
+            int i1 = Character.getNumericValue(c1);
+            int i2 = Character.getNumericValue(c2);
+            int i3 = Character.getNumericValue(c3);
+            //截取字符串三位数
+            String s1 = str.substring(0, 1);
+            String s2 = str.substring(1, 2);
+            String s3 = str.substring(2, 3);
+            //截取字符-->转int
+//            int i1 = Integer.valueOf(s1);
+//            int i2 = Integer.valueOf(s2);
+//            int i3 = Integer.valueOf(s3);
+            //截取数的3次方
+            int b1 = i1 * i1 * i1;
+            int b2 = i2 * i2 * i2;
+            int b3 = i3 * i3 * i3;
+            //相加与原值相比较
             int c4 = b1 + b2 + b3;
-            System.out.println("c1：" + c1);
-            System.out.println("c2：" + c2);
-            System.out.println("c3：" + c3);
-            System.out.println("c4：" + c4);
-            System.out.println("b1：" + b1);
-            System.out.println("b2：" + b2);
-            System.out.println("b3：" + b3);
             if (c4 == Integer.valueOf(str)) {
                 System.out.println("水仙花数：" + str);
             }
