3c4bb9e5506c18e444e5cfd1793e0cd20a3b622a
rainboz <784269332@qq.com>
2019-05-08 14:31:06 +0800

    ［需求／BUG］:基本数据类型
    ［模块］:
    
    ［说明］：

---
+++ src/main/java/com/zhb/LintCode/employees/T1.java
@@ -0,0 +1,11 @@
+package com.zhb.LintCode.employees;
+
+/**
+ * @program: StudyJava
+ * @description:
+ * @author: rainboz
+ * @create: 19-5-8 上午8:44
+ **/
+public class T1 {
+}
+
---
+++ src/main/java/com/zhb/LintCode/employees/T3.java
@@ -0,0 +1,11 @@
+package com.zhb.LintCode.employees;
+
+/**
+ * @program: StudyJava
+ * @description: 字符串相乘
+ * @author: rainboz
+ * @create: 19-5-8 上午8:44
+ **/
+public class T3 {
+}
+
