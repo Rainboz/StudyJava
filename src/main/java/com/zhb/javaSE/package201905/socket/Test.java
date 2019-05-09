d819080cfba539286e23f9a569966cce0d995538
Rainboz <784269332@qq.com>
2019-05-09 20:51:00 +0800

    接口

---
+++ src/main/java/com/zhb/javaSE/package201905/socket/Test.java
@@ -0,0 +1,11 @@
+package com.zhb.javaSE.package201905.socket;
+
+/**
+ * @program: StudyJava
+ * @description:
+ * @author: rainboz
+ * @create: 2019-05-09 18:05
+ **/
+public interface Test {
+    public double a(double m);//对于接口方法，修饰符“public”是多余的
+}
