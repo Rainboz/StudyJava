87dda60cadad61404dde95b060636115cc3490e4
Rainboz <784269332@qq.com>
2019-05-16 08:40:23 +0800

    I/O输出流

---
+++ src/main/java/com/zhb/javaSE/stream_file_io/win10/FileDemo.java
@@ -0,0 +1,47 @@
+package com.zhb.javaSE.stream_file_io.win10;
+
+import java.io.*;
+
+/**
+ * @program: StudyJava
+ * @description:
+ * @author: rainboz
+ * @create: 2019-05-15 10:05
+ **/
+public class FileDemo {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Rainboz\\Desktop\\random_list_name.txt");
+        FileReader fileReader = null;
+        BufferedReader reader = null;
+        InputStreamReader inputStreamReader = null;
+        InputStream inputStream = null;
+        try {
+            fileReader = new FileReader(file);
+            inputStream = new FileInputStream(file);
+            //为什么是GBK,不是UTF8
+            inputStreamReader =  new InputStreamReader(inputStream,"GBK");
+
+            reader = new BufferedReader(inputStreamReader);
+
+            String line;
+            while ((line = reader.readLine())!=null){
+                System.out.println(line);
+            }
+            System.out.println(fileReader.read());
+            System.out.println(fileReader.getEncoding());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileReader.close();
+                reader.close();
+                inputStream.close();
+                inputStreamReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/zhb/javaSE/stream_file_io/win10/FileWriterDemo.java
@@ -0,0 +1,35 @@
+package com.zhb.javaSE.stream_file_io.win10;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @program: StudyJava
+ * @description:
+ * @author: rainboz
+ * @create: 2019-05-15 15:45
+ **/
+public class FileWriterDemo {
+    public static void main(String[] args) throws IOException {
+        //创建输出流对象
+        //FileWriter fw = new FileWriter("src\\main\\resources\\zhb.properties");//getResourceAsStream，默认resources,不知道
+        FileWriter fw = new FileWriter("zhb.txt");//项目根目录
+        /*
+         *创建输出流做了哪些事情：
+         *          A：调用系统资源创建了一个文件
+         *          B：创建输出流对象
+         *          C：把输出流对象指向文件
+         */
+        //调用输出流对象的写数据的方法
+        //写一个字符串数据
+        fw.write("20190515");
+        //字符写不进去，没有直接写到文件，其实是写到了内存缓冲区
+        //刷新缓冲区
+        fw.flush();
+
+        //释放资源
+        //通知系统释放和该文件相关的资源
+        fw.close();
+//        while (true){}
+    }
+}
