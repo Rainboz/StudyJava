8b000881c6194d0129e4aa93b5c06765317b986b
rainboz <784269332@qq.com>
2019-06-26 09:51:22 +0800

    OverView：test
    BUG：
    
    Description：
    
    Author:rainboz

---
+++ src/main/java/com/zhb/javaSE/package_201906/java_first/MainTest.java
@@ -0,0 +1,9 @@
+package com.zhb.javaSE.package_201906.java_first;
+
+public class MainTest {
+    static int sj￥ = 2;
+
+    public static void main(String[] args) {
+        System.out.println(sj￥);
+    }
+}
