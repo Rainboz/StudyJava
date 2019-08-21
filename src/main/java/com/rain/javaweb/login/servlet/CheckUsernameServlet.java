1e541ecac457365239f65f995c346b7ce9566c6d
rainboz <784269332@qq.com>
2019-08-21 15:24:07 +0800

    [功能]:通过js中xhr对象【异步】校验用户名是否存在 [实现]:发送异步请求进行校验-->设置请求信息-->发送请求-->监听readyState的状态-->响应处理完成[4]-->处理正确[200]-->获取服务器响应回来的数据-->将信息显示到register_span中-->跟var msg = xhr.responseText;对应response.getWriter().println(msg);

---
+++ src/main/java/com/rain/javaweb/login/servlet/CheckUsernameServlet.java
@@ -0,0 +1,43 @@
+package com.rain.javaweb.login.servlet;
+
+import com.rain.javaweb.login.beans.User;
+import com.rain.javaweb.login.dao.UserDao;
+import com.rain.javaweb.login.dao.UserDaoImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @progrom StudyJava
+ * @Description TODO
+ * @Author rainboz
+ * @Date 2019-08-21 上午11:31
+ */
+public class CheckUsernameServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //业务：获取用户名
+        String username = request.getParameter("username");
+
+        UserDao userDao = new UserDaoImpl();
+        User user = userDao.getUserByUsername(username);
+        String msg = "";
+        if (user == null) {
+            //可用
+            msg = "用户名可用";
+        } else {
+            msg = "用户名已存在";
+        }
+
+        response.setContentType("text/html;charset=utf-8");
+        //跟var msg = xhr.responseText;对应
+        response.getWriter().println(msg);
+
+    }
+}
